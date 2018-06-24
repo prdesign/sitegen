7a083c725974d02b7c79d67b9cf1db5644e4b269
rcraig12 <rcraig12@gmail.com>
2018-06-24 20:31:09 +0100

    css updates

--- src/resources/views/dashboard/index.blade.php
+++ src/resources/views/dashboard/index.blade.php
@@ -1,6 +1,16 @@
-<h1>Dashboard</h1>
-<ul>
-    <li><a href="">Settings</a></li>
-    <li><a href="{{ route( 'sitegen.page.browse' ) }}">Pages</a></li>
-    <li><a href="{{ route( 'sitegen.formbuilder.browse' ) }}">Form Builder</a></li>
-</ul>
+@extends('sitegen::sitegen.layout')
+@section('title' , 'Dashboard')
+@section('content')
+<div class="admin-page-wrapper centered">
+    <div class="dashboard-menu">
+      <div class="dashboard-title">
+        <h1 class="dashboard-title-h1">Dashboard</h1>
+      </div>
+      <div class="dashboard-links">
+          <a href="#" class="dashboard-link w-button">Settings</a>
+          <a href="{{route('sitegen.page.browse')}}" class="dashboard-link w-button">Pages</a>
+          <a href="{{route('sitegen.form.browse')}}" class="dashboard-link w-button">Forms</a>
+      </div>
+    </div>
+  </div>
+@endsection
